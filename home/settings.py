98e92981a9d685c457f2d3c3abda07bfd06c05ce
NikithaProjects <paleti2019nikitha@gmail.com>
2025-03-27 21:36:37 +0530

    Update settings.py

--- home/settings.py
+++ home/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5rpc3oe)^*975sfa6k%csrqq(=w@umw(-&%1oha!z+zjeh2r*u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
